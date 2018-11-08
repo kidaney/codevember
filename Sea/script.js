9b692759b2568db0e0b438c6582ad988a4907562
Mirandamm228 <codingwith.miranda@gmail.com>
2018-11-08 08:14:45 -0700

    finished setting markers, still figuring out delay

--- Sea/script.js
+++ Sea/script.js
@@ -9,66 +9,75 @@ let anyMarker = document.getElementsByClassName("marker");
 function displayInfo(i){
         info.style.display = "block";
         if(anyMarker[i].id === "mediterranean"){
-            document.getElementById("text").innerHTML = "This is all about the Mediterranean Sea";
+            document.getElementById("text").innerHTML = "This is where the Mediterranean Sea is located. <a href='https://www.britannica.com/place/Mediterranean-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "red"){
-            document.getElementById("text").innerHTML = "This is all about the Red Sea";
+            document.getElementById("text").innerHTML = "This is where the Red Sea is located. <a href='https://www.britannica.com/place/Red-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "black"){
-            document.getElementById("text").innerHTML = "This is all about the Black Sea";
+            document.getElementById("text").innerHTML = "This is where the Black Sea is located. <a href='https://www.britannica.com/place/Black-Sea' target='_blank'>Learn more</a>";
         }
-        if(anyMarker[i].id === "carribean"){
-            document.getElementById("text").innerHTML = "This is all about the Carribean Sea";
+        if(anyMarker[i].id === "caribbean"){
+            document.getElementById("text").innerHTML = "This is where the Carribean Sea is located. <a href='https://www.britannica.com/place/Caribbean-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "gulfMexico"){
-            document.getElementById("text").innerHTML = "This is all about the Gulf of Mexico";
+            document.getElementById("text").innerHTML = "This is where the Gulf of Mexico is located. <a href='https://www.britannica.com/place/Gulf-of-Mexico' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "hudson"){
-            document.getElementById("text").innerHTML = "This is all about the Hudson Bay";
+            document.getElementById("text").innerHTML = "This is where the Hudson Bay is located. <a href='https://www.britannica.com/place/Hudson-Bay' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "bering"){
-            document.getElementById("text").innerHTML = "This is all about the Bering Sea";
+            document.getElementById("text").innerHTML = "This is where the Bering Sea is located. <a href='https://www.britannica.com/place/Bering-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "tasman"){
-            document.getElementById("text").innerHTML = "This is all about the Tasman Sea";
+            document.getElementById("text").innerHTML = "This is where the Tasman Sea is located. <a href='https://www.britannica.com/place/Tasman-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "coral"){
-            document.getElementById("text").innerHTML = "This is all about the Coral Sea";
+            document.getElementById("text").innerHTML = "This is where the Coral Sea is located. <a href='https://www.britannica.com/place/Coral-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "bengal"){
-            document.getElementById("text").innerHTML = "This is all about the Bay of Bengals";
+            document.getElementById("text").innerHTML = "This is where the Bay of Bengals is located. <a href='https://www.britannica.com/place/Bay-of-Bengal' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "arabian"){
-            document.getElementById("text").innerHTML = "This is all about the Arabian Sea";
+            document.getElementById("text").innerHTML = "This is where the Arabian Sea is located. <a href='https://www.britannica.com/place/Arabian-Sea' target='_blank'>Learn more</a>";
         }
         if(anyMarker[i].id === "north"){
-            document.getElementById("text").innerHTML = "This is all about the North Sea";
+            document.getElementById("text").innerHTML = "This is where the North Sea is located. <a href='https://www.britannica.com/place/North-Sea' target='_blank'>Learn more</a>";
         }
 }
 
-function onMouseOver() {
+// function onMouseOver() {
+//     for(let i = 0; i < anyMarker.length; i++){
+//         anyMarker[i].onmouseover = function() {
+//             displayInfo(i);
+//         }
+//     }
+// }
+
+// function onMouseOver() {
     for(let i = 0; i < anyMarker.length; i++){
         anyMarker[i].onmouseover = function() {
-            displayInfo(i);
+            delayMouseover(anyMarker[i], alert("your error lies here"));
+            // displayInfo(i);
         }
     }
-}
+// }
 
 let delayMouseover = function (aMarker, modal) {
     let timeout = null;
-    aMarker.onmouseover = function() {
+    aMarker[i].onmouseover = function() {
         // Set timeout to be a timer which will invoke callback after 1s
-        timeout = setTimeout(modal, 1000);
+        timeout = setTimeout(modal, 5000);
     };
 
-    aMarker.onmouseout = function() {
+    aMarker[i].onmouseout = function() {
         // Clear any timers set to timeout
         clearTimeout(timeout);
     }
 };
 
 
-delayMouseover(anyMarker, onMouseOver());
+
 
 
 
